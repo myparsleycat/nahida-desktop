17c4f7fadfdd8af46150f1fc9365ea1c3d31f4f7
parsleycat <myparsleycat@gmail.com>
2026-05-12 17:55:04 +0900

    fix: rename model viewer memory protocol registrar

--- src/main/protocals/index.ts
+++ src/main/protocals/index.ts
@@ -1,8 +1,8 @@
 import type { NahidaDesktop } from "@main/index";
 import { registerLocalProtocal } from "./local";
-import { registerModelViewerMemoryProtocal } from "./model-viewer-memory";
+import { registerModelViewerMemoryProtocol } from "./model-viewer-memory";
 
 export function registerProtocal(desktop: NahidaDesktop) {
     registerLocalProtocal(desktop);
-    registerModelViewerMemoryProtocal();
+    registerModelViewerMemoryProtocol();
 }
--- src/main/protocals/model-viewer-memory.ts
+++ src/main/protocals/model-viewer-memory.ts
@@ -4,6 +4,6 @@ import {
 } from "@main/services/protocol/model-viewer-memory";
 import { protocol } from "electron";
 
-export function registerModelViewerMemoryProtocal() {
+export function registerModelViewerMemoryProtocol() {
     protocol.handle(MODEL_VIEWER_MEMORY_PROTOCOL, handleModelViewerMemoryProtocol);
 }
